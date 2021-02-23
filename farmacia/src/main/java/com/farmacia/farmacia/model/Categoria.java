3e5f2f01237a0bf3a8c0b8806e06200522a930af
Raphael Scaziti <rscaziti@gmail.com>
2021-02-23 15:06:42 -0300

    Adicionado model de Produto e alterado model de Categoria

--- farmacia/src/main/java/com/farmacia/farmacia/model/Categoria.java
+++ farmacia/src/main/java/com/farmacia/farmacia/model/Categoria.java
@@ -4,9 +4,12 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table(name = "tb_categoria")
@@ -21,6 +24,10 @@ public class Categoria
 	private String nomeCategoria;
 	
 	private boolean ativo;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("categoria")
+	private Produto produto;
 
 	public long getId() {
 		return id;
