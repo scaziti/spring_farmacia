eb49894df3730b30522ba86484fa45f2f37d0cb1
Raphael Scaziti <rscaziti@gmail.com>
2021-02-23 14:53:28 -0300

    Adicionado controller, model e repository

---
+++ farmacia/src/main/java/com/farmacia/farmacia/model/Categoria.java
@@ -0,0 +1,49 @@
+package com.farmacia.farmacia.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_categoria")
+public class Categoria 
+{
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotNull
+	@Size(min = 2, max = 100)
+	private String nomeCategoria;
+	
+	private boolean ativo;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNomeCategoria() {
+		return nomeCategoria;
+	}
+
+	public void setNomeCategoria(String nomeCategoria) {
+		this.nomeCategoria = nomeCategoria;
+	}
+
+	public boolean isAtivo() {
+		return ativo;
+	}
+
+	public void setAtivo(boolean ativo) {
+		this.ativo = ativo;
+	}
+	
+}
